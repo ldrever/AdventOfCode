105c916d5b39fbe563c810811d8b11b33288e0f8
ldrever <lawrence.drever@gmail.com>
2024-12-17 21:18:12 +0000

    passes part 1 and catches part 2 in the act of failing (just flip isPart1)

--- Day15.java
+++ Day15.java
@@ -37,12 +37,12 @@ class Day15 {
 		Scanner sc = new Scanner(System.in);
 
 		for(int i = 0; i < ra.length; i++) {
-			if(i == 655) debug = true;
-			if(debug) {
-				System.out.print("Next move will be " + ra[i] + ". ");
-				System.out.println("Proceed with evolution " + i + "?");
-				String input = sc.next();
-				if(input.equalsIgnoreCase("N")) break;
+			if(i == 656 && !isPart1) debug = true; // FIXME JEB
+			if(debug && !isPart1) { // FIXME JEB
+				System.out.print("Next move will be " + ra[i] + ". "); // FIXME JEB
+				System.out.println("Proceed with evolution " + i + "?"); // FIXME JEB
+				String input = sc.next(); // FIXME JEB
+				if(input.equalsIgnoreCase("N")) break; // FIXME JEB
 			}
 
 			lg.evolve(ra[i], isPart1, debug);
